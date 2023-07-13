b592f05d6512f3fb046e17df79f713211d0cd197
Gabriel-Borgess <devborgespro@gmail.com>
2023-07-13 07:51:51 +0100

    fixing errors and create routes

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,10 @@ export default function Page() {
     return <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
         Descubra, Compartilhe e Explore  <br className="max-md:hidden" />
-        <span className="bg-gradient-to-tr from-cyan-400 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">Ideias de <br
+        <span className="from-cyan-400 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">Ideias de <br
         /> Prompts para Microsaas </span>
         </h1>
-        <p className="desc text-center">A comunidade de prompts para IA, onde você pode inspirar-se, compartilhar suas criações e explorar as ideias mais recentes para impulsionar seu negócio de Microsaas.</p>
+        <p className="desc text-center">A comunidade de prompts para IA, onde você pode inspirar-se, compartilhar suas criações e explorar as ideias mais recentes para impulsionar o seu Microsaas.</p>
         <div>
         
         </div>
